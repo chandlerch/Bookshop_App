7b1a8b9d76adb650c48582987d70fb4f2e461e8f
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-23 16:08:27 -0400

    add delete, post, and put routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 import os
-from . import create_app
-from models import Book
-from flask import jsonify
+from . import create_app, db
+from .models import Book
+from flask import jsonify, request
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
@@ -15,4 +15,49 @@ def get_book(isbn):
     book = Book.query.get(isbn)
     if book is None:
         abort(404)
+    return jsonify(book.to_json())
+
+@app.route("/book/<int:isbn>", methods=["DELETE"])
+def delete_book(isbn):
+    book = Book.query.get(isbn)
+
+    if book is None:
+        abort(404)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return jsonify({'result': True})
+
+@app.route('/book', methods=['POST'])
+def create_book():
+    if not request.json:
+        abort(400)
+
+    book = Book(
+        title=request.json.get('title'),
+        author=request.json.get('author'),
+        price=request.json.get('price')
+    )
+
+    db.session.add(book)
+    db.session.commit()
+
+    return jsonify(book.to_json()), 201
+
+@app.route('/book/<int:isbn>', methods=['PUT'])
+def update_book(isbn):
+    if not request.json:
+        abort(400)
+
+    book = Book.query.get(isbn)
+
+    if book is None:
+        abort(404)
+
+    book.title = request.json.get('title', book.title)
+    book.author = request.json.get('author', book.author)
+    book.price = request.json.get('price', book.price)
+    db.session.commit()
+
     return jsonify(book.to_json())
